f90d1f64afe41617f160d0d1e9b56ffcfcf55491
khaerin <khaerinnisa.cha@gmail.com>
2023-05-09 14:30:11 +0800

    asistensi

--- controllers/MahasiswaController.php
+++ controllers/MahasiswaController.php
@@ -4,14 +4,14 @@ namespace app\controllers;
 
 class MahasiswaController extends \yii\web\Controller
 {
+    public function actionHapusKrs($id)
+    {
+        return $this->render('hapus-krs');
+    }
+
     public function actionIndex()
     {
         return $this->render('index');
-    }
-
-    public function actionProfil()
-    {
-        return $this->render('profil');
     }
 
 }
